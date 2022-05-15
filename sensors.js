a18deaab08a289bd67374d27dcc90790ca172bc0
upperdim <45048352+upperdim@users.noreply.github.com>
2022-05-15 18:17:46 +0300

    pack ray casting code into a private function

--- sensors.js
+++ sensors.js
@@ -1,14 +1,18 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
+        this.rayCount = 5;
         this.rayLength = 100;
-        this.raySpread = Math.PI / 4;
+        this.raySpread = Math.PI / 2;
 
         this.rays = [];
     }
 
     update() {
+        this.#castRays();
+    }
+
+    #castRays() {
         this.rays = [];
 
         for (let i = 0; i < this.rayCount; ++i) {
