81bad41cd71070a421652f41606a32cb5ec7a31a
upperdim <45048352+upperdim@users.noreply.github.com>
2022-05-15 17:45:08 +0300

    generalize road lines more

--- road.js
+++ road.js
@@ -12,13 +12,23 @@ class Road {
         const infinity = 1000000;
         this.top    = -infinity;
         this.bottom =  infinity;
+
+        const topLeft     = {x: this.left,  y: this.top};
+        const topRight    = {x: this.right, y: this.top};
+        const bottomLeft  = {x: this.left,  y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+
+        this.borders = [
+            [topLeft,  bottomLeft],
+            [topRight, bottomRight]
+        ];
     }
 
     getLaneCenter(laneIndex) {
         // Prevent out of bound
         laneIndex = Math.max(laneIndex, 0);
         laneIndex = Math.min(laneIndex, this.laneCount);
-        
+
         const laneWidth = this.width / this.laneCount;
         return (this.left) + (laneIndex * laneWidth) + (laneWidth / 2);
     }
@@ -28,19 +38,24 @@ class Road {
         ctx.strokeStyle = "white";
 
         // Lane strips
-        for (let i = 0; i <= this.laneCount; ++i) {
+        for (let i = 1; i < this.laneCount; ++i) {
             const x = lerp(this.left, this.right, i/this.laneCount); // linear interp
 
-            if (i > 0 && i < this.laneCount) {
-                ctx.setLineDash([20,20]);
-            } else {
-                ctx.setLineDash([]);
-            }
-
+            ctx.setLineDash([20,20]);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        // Border (outer) lines
+        ctx.setLineDash([]);
+        
+        this.borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
     }
 }
